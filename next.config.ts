786f587f8ff4af97489eb3c1cdf8d78fd043c32d
Axel <troncoso.axel.g@hotmail.com>
2026-06-09 15:42:30 -0400

    Adding turbopack and react compiler within the nextjs project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactCompiler: true,
+    /* config options here */
+    experimental: {
+        turbopackFileSystemCacheForDev: true,
+    },
 };
 
 export default nextConfig;
